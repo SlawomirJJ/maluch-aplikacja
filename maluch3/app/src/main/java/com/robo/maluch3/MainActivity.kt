e7c35c19595a565b35ef20a3854e42386788858a
SlawomirJJ <sawomirjedziniak@gmail.com>
2021-03-30 19:05:32 +0200

    dodanie sterowania w trybie RC

--- maluch3/app/src/main/java/com/robo/maluch3/MainActivity.kt
+++ maluch3/app/src/main/java/com/robo/maluch3/MainActivity.kt
@@ -2,10 +2,39 @@ package com.robo.maluch3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var database = FirebaseDatabase.getInstance().reference
+
+
+        btn_przod.setOnClickListener{
+        database.setValue("Przod")
+        }
+
+
+        btn_tyl.setOnClickListener{
+            database.setValue("Tyl")
+        }
+
+        btn_prawo.setOnClickListener{
+            database.setValue("Prawo")
+        }
+
+        btn_lewo.setOnClickListener{
+            database.setValue("Lewo")
+        }
+
+
+
+
     }
 }
